c39c21bc0e7ff05abdcd4795fa69e865f45bcdbf
Carolina Jimenez Vargas. <carolinajimenez020@gmail.com>
2023-10-09 17:56:19 -0500

    se cambio el tipo de contacto a objeto

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
-//declarar la lista de tareas
-
-const taskList = ["Carolina Jiménez", "Arturo Guerra", "Barack Obama", "Jack London"]; 
+const taskList = [
+    {
+        id: 0,
+        nombre: "Carolina",
+        apellido: "Jimenez",
+        telefono: "3137654938",
+        ubicacion: {
+            ciudad: "Sabaneta",
+            direccion: "calle 20 sur"
+        }
+    },
+    {
+        id: 1,
+        nombre: "Arturo",
+        apellido: "Guerra",
+        telefono: "3104738923",
+        ubicacion: {
+            ciudad: "Medellín",
+            direccion: "Calle 10 sur # 39a-250"
+        }
+    },
+    {
+        id: 2,
+        nombre: "Barack",
+        apellido: "Obama",
+        telefono: "3124738923",
+        ubicacion: {
+            ciudad: "Medellín",
+            direccion: "Calle 10 sur # 39a-250"
+        }
+    },
+    {
+        id: 5,
+        nombre: "Miguel",
+        apellido: "de Unamuno",
+        telefono: "3104738923",
+        ubicacion: {
+            ciudad: "Medellín",
+            direccion: "Calle 10 sur # 39a-250"
+        }
+    }
+]; 
 
 // declarar función nuevo contacto
 
-function addContact(name) {
-    taskList.push(name);
+function addContact(contact) {
+    
+    const {nombre, apellido, telefono, ubicacion} = contact;
+    // const nombre = contact.nombre;
+    // const apellido = contact.apellido;
+    // { nombre, apellido} = contact
+    const lastElementId = taskList.length -1;
+    const newId = taskList[lastElementId].id +1;
+
+
+    taskList.push({
+        id: newId,
+        nombre,
+        apellido,
+        telefono,
+        ubicacion
+    });
 };
 
 // borrar un contacto
 
 function deleteContact(id) {
-    taskList.splice(id, 1);
+    // taskList.splice(id, 1);
+    const index = taskList.map(task => task.id).indexOf(id);
+    console.log("index", index);
+    taskList.splice(index, 1);
 };
 
 function deleteContactByName(name) {
@@ -29,9 +86,10 @@ function deleteContactByName(name) {
 // imprimir en consola los contacto presentes en la lista
 
 function showContacts() {
-    console.log("*****LISTA DE TAREAS*****")
+    console.log("*****LISTA DE CONTACTOS*****")
 
     taskList.map((task, id) => {
-        console.log(`${id+1}. ${task}`);
+        console.log(`${id+1}. ${task.nombre} ${task.apellido}`);
     });
 };
+
